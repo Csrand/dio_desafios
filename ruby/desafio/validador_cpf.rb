888bc20db6aba7738736a28b370866ae7d399ad0
Csrand <chrystiansenna@gmail.com>
2025-07-10 14:23:20 -0300

    desafio concluido

--- ruby/desafio/validador_cpf.rb
+++ ruby/desafio/validador_cpf.rb
@@ -1,1 +1,11 @@
+require 'cpf_cnpj'
 
+puts "Digite o número do CPF para validação:"
+
+cpf_digitado = gets.chomp
+
+if CPF.valid?(cpf_digitado)
+  puts "O CPF digitado é válido!"
+else
+  puts "O CPF digitado é inválido."
+end
